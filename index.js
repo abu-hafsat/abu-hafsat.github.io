fb9d23cd9a117dcfceef90a449e8ebcf431fd9a6
abu-hafsat <162338375+abu-hafsat@users.noreply.github.com>
2024-06-17 06:45:05 -0700

    Add files via upload
    adding the html and css files

---
+++ index.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "230px";
+    x.style.width = "170px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "150px";
+  }
